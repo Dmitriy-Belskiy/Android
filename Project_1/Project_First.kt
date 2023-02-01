80b66d5c8ca95db8b1358dedf294ba7e3e218cf4
Dmitriy-Belskiy <bdima3006@mail.ru>
2023-02-01 11:26:43 +0300

    Update Project_First.kt

--- Project_1/Project_First.kt
+++ Project_1/Project_First.kt
@@ -8,29 +8,6 @@ fun main(args: Array<String>) {
 		println(element)    
 	}
 }
-
-class Cat (var weight: Int = 0, var meow: Int = 0, var weightPlus: Int = 0, var meowPlus: Int = 0) {
-	fun Feeding(): String{
-		var weightCycle = weightPlus + weight
-		while (weight < weightCycle){
-			weight += 1
-		}
-		return "After Feeding: $weight"
-	}
-	
-	fun FindWeight(): String{
-		return "Weight: $weight"
-	}
-	
-	fun MeowDeath(): String{
-		var meowCycle = meowPlus + meow
-		while (meow < meowCycle) {
-			meow += 1
-		}
-		if (meow >= 10) {
-			return "Meow Death: over $meow"
-		} else {
-			return "Meow: $meow"
-		}
-	}
-}
+fun FindWeight(): String{
+	return "Weight: $weight"
+}	

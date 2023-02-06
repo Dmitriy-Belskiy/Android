f896533c0ec91904d8b99aafe2321bb09c10139b
Dmitriy-Belskiy <bdima3006@mail.ru>
2023-02-06 11:20:13 +0300

    Create Cat.kt

---
+++ Projects/Cat.kt
@@ -0,0 +1,33 @@
+class Cat
+{
+    private Double originWeight;
+    private Double weight;
+    
+     public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+
+    }
+    
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+    
+    
+      public Double getFoodWheigh() {
+        Double result = weight - originWeight;
+        return (result < 0) ? 0 : result;
+    }
+      
+       public void pee() {
+        if (weight <= originWeight) {
+            System.out.println("I do not want pee");
+        } else {
+            weight = weight - 1;
+            System.out.println("Ehh, I wanna");
+        }
+    }
+      
+}

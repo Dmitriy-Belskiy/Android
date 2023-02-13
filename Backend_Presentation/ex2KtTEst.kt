6a7e700440179d0df569ba6e86c6a4a9e5be91d9
Dmitriy-Belskiy <bdima3006@mail.ru>
2023-02-13 12:12:18 +0300

    Update ex2KtTEst.kt

--- Backend_Presentation/ex2KtTEst.kt
+++ Backend_Presentation/ex2KtTEst.kt
@@ -6,10 +6,10 @@ class Rectangle(val width: Float, val height: Float) : Figure(perimeter, area)
 {
     override fun perimeter(): Float{
         return width * 2 + height * 2;
-        printl("Perimeter of this rectangle is $perimeter")
+        println("Perimeter of this rectangle is $perimeter")
     }
     override fun area(): Float{
         return width * height;
-        printl("Area of this rectangle is $area")
+        println("Area of this rectangle is $area")
     }
 }

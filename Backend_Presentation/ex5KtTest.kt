80602def61b4ee7d40015b71916be442932006e4
Dmitriy-Belskiy <bdima3006@mail.ru>
2023-02-13 12:31:54 +0300

    Create ex5KtTest.kt

---
+++ Backend_Presentation/ex5KtTest.kt
@@ -0,0 +1,18 @@
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+class Rectangle(val width: Float, val height: Float) : Figure(perimeter, area)
+{
+    override fun perimeter(): Float{
+        return width * 2 + height * 2;
+        println("Perimeter of this rectangle is $perimeter")
+    }
+    override fun area(): Float{
+        return width * height;
+        println("Area of this rectangle is $area")
+    }
+}
+
+fun areaDiff(rectangleA: Rectangle, rectangleB: Rectangle) : Float = 
+    rectangleA.area() - rectangleB.area()

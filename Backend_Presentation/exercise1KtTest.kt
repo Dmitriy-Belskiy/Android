e7b27974ae3c5ab4d2e33066213ac0964f44f0f1
Dmitriy-Belskiy <bdima3006@mail.ru>
2023-02-13 10:55:19 +0300

    Create exercise1KtTest.kt

---
+++ Backend_Presentation/exercise1KtTest.kt
@@ -0,0 +1,17 @@
+class Rectangle
+constructor(
+    val height: Int,
+    val width: Int
+) 
+
+fun perimeter(): Int {
+     return width * 2 + height * 2;
+}
+operator fun squer(): Int {
+    return width * height
+}
+
+
+
+
+

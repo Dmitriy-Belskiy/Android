fc2d23e33baefd05316ce74b9523117e81577994
Dmitriy-Belskiy <bdima3006@mail.ru>
2023-02-13 12:33:59 +0300

    Update ex5KtTest.kt

--- Backend_Presentation/ex5KtTest.kt
+++ Backend_Presentation/ex5KtTest.kt
@@ -16,3 +16,7 @@ class Rectangle(val width: Float, val height: Float) : Figure(perimeter, area)
 
 fun areaDiff(rectangleA: Rectangle, rectangleB: Rectangle) : Float = 
     rectangleA.area() - rectangleB.area()
+
+
+//Подставление значений
+// val diff = areaDiff(Rectangle(10.0, 20.0), ScalableRectangle(5.0, 10.0))
